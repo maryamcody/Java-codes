5cf2cbb446f4cc79ff974af96d011359bfd6173a
maryamcody <maryamabdulhakim2010@gmail.com>
2026-07-16 11:45:43 +0300

    intro java

---
+++ intro.java
@@ -0,0 +1,18 @@
+class Main{
+        public static void main (String[] args){
+        System.out.println("Hey java");
+        int number1 =20;
+        int number2 =10;
+        int sum = number1 + number2;
+        System.out.println(sum);
+        int number3 =30;
+        int number4=20;
+        int sub = number3 - number4;
+        
+        System.out.println(sub);
+
+    }
+    }
+
+    
+
