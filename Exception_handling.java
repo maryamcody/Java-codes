2d619fd942459a2d95be621006e7bfd5c6f9f4e7
maryamcody <maryamabdulhakim2010@gmail.com>
2026-08-18 11:00:47 +0300

    Add files via upload

---
+++ Exception_handling.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exception_handling {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        try {
+            System.out.println("Enter two numbers");
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=x/y;
+            System.out.println(x+"/"+y+"="+z);
+        } catch (ArithmeticException ex) {
+            System.out.println("--- catch block ---");
+            System.out.println(ex.toString());
+
+        }finally {
+            System.out.println("--- finally block ---");
+            System.out.println("Application Designed & Developed by");
+            System.out.println("team @ Codingal");
+            sc.close();
+        }
+        System.out.println("--- Done ---");
+    }
+}
